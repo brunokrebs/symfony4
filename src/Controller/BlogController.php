39265440c8f94081660fc3b4fa54fabc0edddbc0
Bruno Krebs <bruno.krebs@auth0.com>
2018-02-26 15:53:37 -0300

    lot of stuff;

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,18 +2,52 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends Controller
 {
+    /** @var integer */
+    const POST_LIMIT = 5;
+
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $authorRepository;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $blogPostRepository;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->blogPostRepository = $entityManager->getRepository('App:BlogPost');
+        $this->authorRepository = $entityManager->getRepository('App:Author');
+    }
+
     /**
      * @Route("/", name="homepage")
+     * @Route("/entries", name="entries")
      */
-    public function indexAction()
+    public function entriesAction(Request $request)
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+        $page = 1;
+
+        if ($request->get('page')) {
+            $page = $request->get('page');
+        }
+
+        return $this->render('blog/entries.html.twig', [
+            'blogPosts' => $this->blogPostRepository->getAllPosts($page, self::POST_LIMIT),
+            'totalBlogPosts' => $this->blogPostRepository->getPostCount(),
+            'page' => $page,
+            'entryLimit' => self::POST_LIMIT
         ]);
     }
 }
